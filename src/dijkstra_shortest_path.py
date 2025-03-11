71fc9df83cce930b5451fc161ceb53d6fe0f6667
uniquehorn79 <uniquehorn79@users.noreply.github.com>
2025-03-11 15:56:32 +0000

    Fix path distance calculation in Dijkstra's algorithm

--- src/dijkstra_shortest_path.py
+++ src/dijkstra_shortest_path.py
@@ -38,10 +38,13 @@ def dijkstra_shortest_path(graph: Dict[str, Dict[str, int]], start: str, end: st
         # If we've reached the end node, reconstruct and return the path
         if current_node == end:
             path = []
+            path_distance = 0
             while current_node:
                 path.append(current_node)
+                if previous_nodes[current_node] is not None:
+                    path_distance += graph[previous_nodes[current_node]][current_node]
                 current_node = previous_nodes[current_node]
-            return list(reversed(path)), current_distance
+            return list(reversed(path)), path_distance
         
         # If we've found a longer path, skip
         if current_distance > distances[current_node]:
